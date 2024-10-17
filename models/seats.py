a544c7f04c345b78350751c86c6049a25a1b9576
JarlDark <dr311682@gmail.com>
2024-10-17 17:12:11 -0300

    Update seats.py

--- models/seats.py
+++ models/seats.py
@@ -1,3 +1,3 @@
-class Seat():
-    def __init__(self):
-        self.numSeat 
+class Seat(): #Se crea una clase.
+    def __init__(self): #Define el método constructor de esta clase, para la implementacion de una instancia.
+        self.numSeat #Esta propiedad representa el número de asientos.
