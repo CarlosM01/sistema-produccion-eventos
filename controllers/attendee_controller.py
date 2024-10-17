58b2150603dd6d00c3775a235da789758fe97dcc
JarlDark <dr311682@gmail.com>
2024-10-17 19:02:27 -0300

    Update attendee_controller.py

--- controllers/attendee_controller.py
+++ controllers/attendee_controller.py
@@ -1,14 +1,14 @@
-from models.attendees import AttendeeModel
-from views.attendee import Attendee
-from views.common import Common
+from models.attendees import AttendeeModel               #Se importa la clase AttendeeModel.
+from views.attendee import Attendee                      #Se importa la clase Attendee.
+from views.common import Common                          #Se importa la clase Common.
 
-class AttendeeController:
-    def __init__(self):
-        self.attendee_model = AttendeeModel()
-        self.attendee_view = Attendee()
-        self.common_view = Common()
+class AttendeeController: #Crea la clase AttendeeController, es funciona como controlador MVC.
+    def __init__(self):   #Constructor de la clase, se ejecuta para crear una instancia del controlador.
+        self.attendee_model = AttendeeModel() #Crea una instancia del modelo de asistentes (AttendeeModel), para la gestionar la lógica de los mismos.
+        self.attendee_view = Attendee()       #Crea una instancia de la vista (Attendee), para manejar la interfaz de los asistentes.
+        self.common_view = Common()           #Crea una instancia de la vista (Common), que maneja funciones comunes como los registros y alertas.
 
-    def register(self):
-        data = self.common_view.register()
-        result = self.attendee_model.register(data)        
-        self.common_view.alert(result['message'])
+    def register(self): #Este metodo servira para registrar asistentes
+        data = self.common_view.register()             #Invoca al método "register" de la vista "Common", que recopilara datos del usuario.
+        result = self.attendee_model.register(data)    #Envía los datos recopilados al modelo "AttendeeModel" para registrar al asistente en la base de datos.
+        self.common_view.alert(result['message'])      #Invoca al método "alert" de la "Common" para mostrar un mensaje, basado en el resultado del registro.
