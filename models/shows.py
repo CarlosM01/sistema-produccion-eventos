499ab4a74e47738238db0f64f4017c5088890707
JarlDark <dr311682@gmail.com>
2024-10-17 17:21:10 -0300

    Update shows.py

--- models/shows.py
+++ models/shows.py
@@ -1,5 +1,5 @@
-class Shows():
-    def __init__(self) -> None:
-        self.location
-        self.date
-        self.capacity
+class Shows(): #Se crea la clase Shows.
+    def __init__(self, location, date, capacity) -> None: #Recibe los parametros "location, date y capacity".
+        self.location = location #Se asigna el valor de location a self.location.
+        self.date = date #Se asigna el valor de date a self.date. 
+        self.capacity = capacity #Se asigna el valor de capacity a self.capacity  
