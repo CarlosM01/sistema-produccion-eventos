4c7fcbf71470f89dc15975300b2f7073332457c2
JarlDark <dr311682@gmail.com>
2024-10-18 20:14:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from controllers.main_controller import MainController
-from initializer import initial_data
+from controllers.main_controller import MainController     #Importa el controlador principal "MainController".
+from initializer import initial_data                       #Importa la función initial_data().
 
-def main():
-    initial_data()
-    main_controller = MainController()
-    main_controller.start()
+def main():                                    #Esta función ejecutara la lógica principal del sistema
+    initial_data()                             #Invoca la esta función para asegurar que la base de datos se ejecuta correctamente.
+    main_controller = MainController()         #Esta intancia creada gestionará el flujo principal de la aplicación.
+    main_controller.start()                    #Invoca el este método para iniciar el ciclo principal del programa, para que el usuario interactue con los menús y funcionalidades.
     
     
-if __name__ == "__main__":
+if __name__ == "__main__":                    #Verifica que todo se ejecute correctamente llamando a "main".
     main()
