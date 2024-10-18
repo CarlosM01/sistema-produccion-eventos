1073bfa4858f9d1dbc96dae3f62ddb51b72001ce
JarlDark <dr311682@gmail.com>
2024-10-18 20:06:37 -0300

    Update initializer.py

--- initializer.py
+++ initializer.py
@@ -1,21 +1,21 @@
-from models.roles import RoleModel
-from models.root import RootModel
+from models.roles import RoleModel            #Importa la clase "RoleModel".
+from models.root import RootModel             #Importa la clase "RootModel".
 
-def initial_data():
-    role_model = RoleModel()
-    root_model = RootModel()
+def initial_data():                           #Esta función agrega a la base de datos con datos iniciales.
+    role_model = RoleModel()                  #Crea un instancia llamada "RoleModel" para luego interactuar con las tablas en la base de datos.
+    root_model = RootModel()                  #Crea un instancia llamada "RootModel" para luego interactuar con las tablas en la base de datos
 
-    roles = ['root', 'admin', 'attendee']
-    for role in roles:
-        role_model.create_role({'role_name': role})
+    roles = ['root', 'admin', 'attendee']     #Define una lista de roles que seran añadidos a la tabla "roles".
+    for role in roles:                        #Itera sobre la lista de roles: 
+        role_model.create_role({'role_name': role}) #Para agregar cada rol en la base de datos, si no existe.
 
-    sudo_email = 'root@example.com'
-    existing_user = root_model.get_by_attribute('email', sudo_email)
-    if not existing_user:
+    sudo_email = 'root@example.com'           #Defien el email del superusuario "root", para verificar si ya existe un usuario ya registrado.
+    existing_user = root_model.get_by_attribute('email', sudo_email)  #Busca en la base de datos si existe el usuario.
+    if not existing_user:                     #Si no existe entonces, se procede a registrar uno nuevo.
         sudo = {
             'name': 'Super User',
             'email': sudo_email,
             'phone': '123456789',
             'password': 'root',
         }
-        root_model.register(sudo)
+        root_model.register(sudo)            #Utiliza el método "register" para registrar al nuevo usuario "root".
