663e9750c53670c1a66eb0b843e28e81d2a31d41
JarlDark <dr311682@gmail.com>
2024-10-18 19:56:39 -0300

    Update db.py

--- db.py
+++ db.py
@@ -1,26 +1,26 @@
-import sqlite3
+import sqlite3            #Importa el método "sqlite3".
 
-class DataBase:
-    def connect(self):
-        try:
-            self.con = sqlite3.connect('dataBase.db')
-            self.cur = self.con.cursor()
-        except sqlite3.Error as e:
-            print(f"Error connecting to database: {e}")
+class DataBase:           #Define la clase "DataBase" que encapsula la lógica para gestionar una base de datos SQLite.
+    def connect(self):    #Se define este método para establecer una conexión con la base de datos.
+        try:              #El bloque "try" contiene:
+            self.con = sqlite3.connect('dataBase.db')   #La conexión con la base de datos
+            self.cur = self.con.cursor()                #Se crea este cursor para ejecutar consultas SQL.
+        except sqlite3.Error as e:                      #Si hay un error durante la conexión, se detecta y muestra un mensaje de error
+            print(f"Error connecting to database: {e}") #Este es el mensaje.
 
-    def SQL(self, query, params=()):
-        try:
-            self.cur.execute(query, params)
-            if query.startswith('SELECT'):
-                columns = [column[0] for column in self.cur.description]
-                result = [dict(zip(columns, row)) for row in self.cur.fetchall()]
-                return result
-            self.con.commit()
-        except sqlite3.Error as e:
-            print(f"Error executing query: {e}")
+    def SQL(self, query, params=()):                    #Define el método "SQL", ademas de los parametros "query" (La consulta SQL) y "params"(Los parámetros de la consulta).
+        try:                                            #Este bloque contiene.
+            self.cur.execute(query, params)             #Intenta ejecutar la consulta "SQL" usando el cursor "self.cur" antes creado.
+            if query.startswith('SELECT'):              #Si la consulta es un "Select" entonces:
+                columns = [column[0] for column in self.cur.description]             #Obtiene las columnas de la tabla solicitada.
+                result = [dict(zip(columns, row)) for row in self.cur.fetchall()]    #Cada fila de "fetchall()" se convierte en un diccionario.
+                return result                                                        #Muestra el resultado.
+            self.con.commit()                              #Si la consulta no es tipo "Select" se ejecuta "commit()"
+        except sqlite3.Error as e:                         #Si ocurre un error al ejecutar la consulta, se muestra un mensaje de error.   
+            print(f"Error executing query: {e}")           #Este es el mensaje.
 
-    def close(self):
-        try:
-            self.con.close()
-        except sqlite3.Error as e:
-            print(f"Error closing the connection: {e}")
+    def close(self):                                       #Este método cierra la conexión la base de datos.
+        try:                                              
+            self.con.close()                                
+        except sqlite3.Error as e:                         #Intenta cerra la conexión con la base de datos, si se logra muestra un mensaje.
+            print(f"Error closing the connection: {e}")    #Este es el mensaje.
