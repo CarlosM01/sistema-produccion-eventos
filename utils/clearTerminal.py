b31dac64396720e94bdc47745508b9cda897ad29
JarlDark <dr311682@gmail.com>
2024-10-18 19:22:55 -0300

    Update clearTerminal.py

--- utils/clearTerminal.py
+++ utils/clearTerminal.py
@@ -1,10 +1,10 @@
-import os
+import os                         #Importa el modulo "os".
 
-def clear_terminal():
-    # Windows
-    if os.name == 'nt':
-        os.system('cls')
-    # Unix/Linux/Mac
-    else:
-        os.system('clear')
+def clear_terminal():             #Define la función capaz de limpiar la terminal.
+    # Windows                     
+    if os.name == 'nt':           #Se agrega para identificar el nombre del sistema operativo, si es windows, "os.name" devuelve "nt".
+        os.system('cls')          #Si el sistema es windows se ejecuta el comando "cls" usando "os.system()" que limpia la terminal
+    # Unix/Linux/Mac            
+    else:                         #Si el sistema operativo es Unix, Linux o Mac, se ejecutara lo siguiente:
+        os.system('clear')        #El comando "clear" limpiara la terminal de estos sistemas operativos.
 
